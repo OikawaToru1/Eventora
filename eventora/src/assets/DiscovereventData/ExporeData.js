b5b73bd24c817e1120da2f0d2e52eab271fc8ffb
Aniket <aniketadhikari2@gmail.com>
2025-01-09 14:33:45 +0545

    created a js file to store temporary data files

---
+++ eventora/src/assets/DiscovereventData/ExporeData.js
@@ -0,0 +1,72 @@
+import musicImage from './music.png'
+import holidayImage from './holidays.png'
+import hobbieImage from './hobbies.png'
+import businessImage from './business.png'
+import nightlifeImage from './nightlife.png'
+ 
+
+const IconsData = [
+    {
+        id:crypto.randomUUID(),
+        IconImage:musicImage,
+        title:'Music',
+    },
+    {
+        id:crypto.randomUUID(),
+        IconImage:holidayImage,
+        title:'Holidays',
+    },
+    {
+        id:crypto.randomUUID(),
+        IconImage:hobbieImage,
+        title:'Hobbies',
+    },
+    {
+        id:crypto.randomUUID(),
+        IconImage:businessImage,
+        title:'business',
+    },
+    {
+        id:crypto.randomUUID(),
+        IconImage:nightlifeImage,
+        title:'nightLife',
+    },
+    {
+        id:crypto.randomUUID(),
+        IconImage:musicImage,
+        title:'Music',
+    },
+    {
+        id:crypto.randomUUID(),
+        IconImage:holidayImage,
+        title:'Holidays',
+    },
+    {
+        id:crypto.randomUUID(),
+        IconImage:hobbieImage,
+        title:'Hobbies',
+    },
+    {
+        id:crypto.randomUUID(),
+        IconImage:businessImage,
+        title:'business',
+    },
+    {
+        id:crypto.randomUUID(),
+        IconImage:nightlifeImage,
+        title:'nightLife',
+    },  
+]
+
+const getIconsData = () =>{
+    return IconsData
+}
+export default getIconsData
+
+
+
+// function getIconsImage(name){
+//     return new URL(`../assests/Explore_icons/${name}`,import.meta.url).href
+// }
+
+// export {getIconsImage}
