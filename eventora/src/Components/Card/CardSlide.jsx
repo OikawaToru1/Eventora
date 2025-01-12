9a550d45ed890b2e832caa5f4ab4a55e20a6f2db
Chandan981 <chandantiwari8889@gmail.com>
2025-01-12 12:01:06 +0545

    Added cardslide component design

--- eventora/src/Components/Card/CardSlide.jsx
+++ eventora/src/Components/Card/CardSlide.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import Card from './Card'
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
 function CardSlide() {
+  const slideLeft = () => {
+    let slider = document.getElementById('sliderDis');
+    slider.scrollLeft = slider.scrollLeft - 250;
+  }
+
+  const slideRight = () => {
+    let slider = document.getElementById('sliderDis');
+    slider.scrollLeft = slider.scrollLeft + 250;
+  }
   return (
-    <div className='w-full flex'>
+    <div className='w-[100%] flex '>
+      <div onClick={slideLeft} className="z-10 absolute top-[42%] left-0 w-[32px] h-[54px] rounded-e-[50px] cursor-pointer transition-all"><span className="text-[32px] font-medium absolute top-[13px]"><FaChevronLeft /></span></div>
+      <div onClick={slideRight} className="z-10 absolute top-[42%] right-0 w-[32px] h-[54px] rounded-s-[50px] cursor-pointer  transition-all"><span className="absolute top-[13px] text-[32px] font-medium  "><FaChevronRight /></span></div>
+      <div className=" w-[100%] h-[100%] overflow-x-scroll scroll-smooth whitespace-nowrap scrollbar-hide flex lg:justify-center mx-auto" id="sliderDis">
         <Card/>
         <Card/>
         <Card/>
         <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
     </div>
   )
 }
