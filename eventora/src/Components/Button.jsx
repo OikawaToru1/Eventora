5c23b724c899da85d29b2257049726e0c277318e
Chandan981 <chandantiwari8889@gmail.com>
2025-01-13 14:38:48 +0545

    added button component for signup and login

--- eventora/src/Components/Button.jsx
+++ eventora/src/Components/Button.jsx
@@ -4,13 +4,16 @@ function Button({name,
   className = ""
 }) {
   return (
-   <button
-   className={`${className} px-3 py-1 bg-black text-white
-   rounded-xl mt-2 `}
+    <div className='w-[80%] mx-auto flex justify-center'>
+        <button
+   className={`${className} px-3 py-2 w-full lg:w-[90%] bg-gradient-to-r from-[#9232b8b4] to-[#cb7deb] hover: text-black
+   rounded-3xl mt-2 font-medium`}
    type='submit'
    >
     {name}
    </button>
+    </div>
+   
   )
 }
 
