757e1244bc22c33c6270c2b8b69490da59c9da48
Chandan981 <chandantiwari8889@gmail.com>
2025-01-13 14:41:21 +0545

    added the inputs component for login and signup

--- eventora/src/Components/Input.jsx
+++ eventora/src/Components/Input.jsx
@@ -10,19 +10,19 @@ const Input = React.forwardRef( function Input({
     
 
     return (
-        <div className='w-full'>
-            {label && <label className='inline-block mb-1 pl-1' 
+        <div className='w-[80%] flex flex-col mx-auto my-5 lg:pl-5'>
+            {label && <label className='text-xl indent-2 text-[#fff] font-medium ' 
             htmlFor={id}>
                 {label}
                 </label>}
                 <input type={type}
-                className= {`px-3 py-2 rounded-lg bg-white text-black outline-none
-                     focus:bg-gray-50 duration-200 border border-gray-200 w-full 
+                className= {`px-3 py-2 rounded-lg bg-transparent text-white outline-none
+                      font-medium border border-gray-200 w-[100%] lg:w-[95%] placeholder:text-[#d3d3d3]
                      ${className}`}
                      ref={ref}
                      {...props}
                      id={id}
-                ></input>
+                ></input>  
         </div>
     )
 })
