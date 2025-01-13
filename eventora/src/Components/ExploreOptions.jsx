ae7f15bf77b6b94aa5eec7dbc9130dee17a5bbfd
Chandan981 <chandantiwari8889@gmail.com>
2025-01-13 14:40:24 +0545

    removed the unused id

--- eventora/src/Components/ExploreOptions.jsx
+++ eventora/src/Components/ExploreOptions.jsx
@@ -7,7 +7,7 @@ function ExploreOptions() {
     return (
             
             <div className=" w-[100%] h-[180px] flex items-center pt-[20px] text-[#2d2d2e] relative ">
-                <div className=" w-[100%] h-[100%] overflow-x-scroll scroll-smooth whitespace-nowrap scrollbar-hide pr-2" id="slider">
+                <div className=" w-[100%] h-[100%] overflow-x-scroll scroll-smooth whitespace-nowrap scrollbar-hide pr-2">
                     {getIcons.map(icon =>(
                         <div key={icon.id} className=" h-[100%] inline-block py-7 px-5 mx-2  text-[16px]">
                             <div className="border-[1px] border-[#2d2d2e] rounded-[50%] w-[68px] h-[68px] flex items-center justify-center hover:drop-shadow-[0_2px_2px_rgba(0,0,0,0.7)] cursor-pointer hover:scale-110 transition-all ">
