229019c815e8492fe19f4e1a48817cf12f700056
Chandan981 <chandantiwari8889@gmail.com>
2025-01-13 14:39:42 +0545

    removed the error for key

--- eventora/src/Components/Card/Card.jsx
+++ eventora/src/Components/Card/Card.jsx
@@ -9,8 +9,8 @@ function Card() {
   return (
       <div className="text-[19px] w-[100%] h-[480px] flex justify-evenly items-center relative px-auto">
           {getEventData.map(item => (
-            <div className=" h-[100%] inline-block p-7 lg:p-6" >
-              <div className="hover:drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)] rounded-t-[16px] rounded-b-[16px] transition-all cursor-pointer" key={item.id} >
+            <div className=" h-[100%] inline-block p-7 lg:p-6" key={item.id} >
+              <div className="hover:drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)] rounded-t-[16px] rounded-b-[16px] transition-all cursor-pointer" >
                 <img src={item.discoverImage} alt={item.imageTitle} className="w-[220px] h-[190px] object-cover rounded-t-[16px]" />
                 <div className="bg-[#f3f3f3] w-[220px] rounded-b-[16px] pl-3 py-3">
                   <span className="underlineGray">{item.imageTitle}</span>
