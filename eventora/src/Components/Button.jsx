7a24b8bcfe0407064dc3c0e61b3e65d539dda049
Chandan981 <chandantiwari8889@gmail.com>
2025-01-16 13:30:51 +0545

    Added a children in button and changed the background and added hover effects

--- eventora/src/Components/Button.jsx
+++ eventora/src/Components/Button.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
 function Button({name,
-  className = ""
+  className = "",
+  children
 }) {
   return (
     <div className='w-[80%] mx-auto flex justify-center'>
         <button
-   className={`${className} px-3 py-2 w-full lg:w-[90%] bg-gradient-to-r from-[#9232b8b4] to-[#cb7deb] hover: text-black
-   rounded-3xl mt-2 font-medium`}
+   className={`${className} px-3 py-2 w-full lg:w-[90%] bg-gradient-to-r from-[#9232b8b4] to-[#cb7deb] text-white
+   rounded-3xl mt-2 font-medium hover:text-[#9232b8b4] hover:bg-gradient-to-r hover:from-[#ffffff] hover:to-[#ffffff] hover:border hover:border-[#9232b8b4] `}
    type='submit'
    >
     {name}
+    {children}
    </button>
     </div>
    
