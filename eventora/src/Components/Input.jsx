4234e6b3886ac96b2cb0b6d4447bc75fbe5dca20
Aniket <aniketadhikari2@gmail.com>
2025-01-16 14:09:33 +0545

    added styles

--- eventora/src/Components/Input.jsx
+++ eventora/src/Components/Input.jsx
@@ -11,18 +11,18 @@ const Input = React.forwardRef( function Input({
 
     return (
         <div className='w-[80%] flex flex-col mx-auto my-5 lg:pl-5'>
-            {label && <label className='text-xl indent-2 text-[#fff] font-medium ' 
+            {label && <label className='text-xl indent-2  font-medium ' 
             htmlFor={id}>
                 {label}
                 </label>}
                 <input type={type}
-                className= {`px-3 py-2 rounded-lg bg-transparent text-white outline-none
+                className= {`px-3 py-2 rounded-lg bg-transparent text-black outline-none
                       font-medium border border-gray-200 w-[100%] lg:w-[95%] placeholder:text-[#d3d3d3]
                      ${className}`}
                      ref={ref}
                      {...props}
                      id={id}
-                ></input>  
+                /> 
         </div>
     )
 })
