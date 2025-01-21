3ea48f532b5ad9e40b68d0766254d550dc994311
Aniket <aniketadhikari2@gmail.com>
2025-01-21 17:29:17 +0545

    merged with ct branch

--- eventora/src/Components/Aboutus.jsx
+++ eventora/src/Components/Aboutus.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Aboutus() {
   return (
-    <div>Aboutus</div>
+    <div className='w-full mt-7'>
+      <div className='w-[70%] mx-auto text-[20px] lg:text-2xl flex flex-col gap-3 '>
+        <span>
+        Welcome to <b> Eventora</b>, your ultimate destination for discovering and experiencing unforgettable events. Whether you're seeking live music, exciting festivals, or professional conferences, we are here to connect you with the moments that matter.
+        </span>
+        <span>
+        Our mission is to bring people together through extraordinary events. We believe in the power of connection and strive to make every event accessible, enjoyable, and unforgettable for everyone
+        </span>
+        <span>
+        <b>Eventora</b> was founded in 2024 with a passion for helping people discover experiences they’ll cherish forever. It started with a small team of enthusiasts who understood the thrill of being part of something special.
+        </span>
+      </div>
+      <div className='border-t border-[#a7a7a7]'>
+        <h2 className='font-bold text-3xl'>Our Team:</h2>
+      </div>
+    </div>
   )
 }
 
--- eventora/src/Components/PostCard.jsx
+++ eventora/src/Components/PostCard.jsx
@@ -24,7 +24,7 @@ function PostCard({$id, title, featuredImage,location}) {
         <div className="text-[19px] w-[100%] h-[480px] flex justify-evenly items-center relative px-auto">
             <div className=" h-[100%] inline-block p-7 lg:p-6"  >
               <div className="hover:drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)] rounded-t-[16px] rounded-b-[16px] transition-all cursor-pointer" >
-                <img src={imageUrl ? imageUrl : altImagae } alt={title} className="w-[220px] h-[190px] object-cover rounded-t-[16px]" />
+                <img src={imageUrl ? altImagae : altImagae } alt={title} className="w-[220px] h-[190px] object-cover rounded-t-[16px]" />
                 <div className="bg-[#f3f3f3] w-[220px] rounded-b-[16px] pl-3 py-3">
                   <span className="underlineGray">{title}</span>
                   <div className="flex items-center gap-2">
