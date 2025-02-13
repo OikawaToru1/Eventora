26bd3040e6e2f78c1d0a2a396eee80d21c24095d
Aniket <aniketadhikari2@gmail.com>
2025-02-13 14:18:02 +0545

    temporary file

--- eventora/src/Components/EventOrganized.jsx
+++ eventora/src/Components/EventOrganized.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import Profile from "../assets/profile2.png"
 
 
-function EventOrganized() {
+function EventOrganized({organizer}) {
   return (
     <>
-    <div className='bg-[#d9d9d9] w-[400px] h-[320px] py-1 flex flex-col justify-center px-4 mt-9 rounded-[16px]'>
+    <div className='bg-[#d9d9d9] w-[320px] h-[100px] py-1 flex flex-col justify-center px-4 mt-9 rounded-[16px]'>
         <div className='flex gap-2'>
             <img src={Profile} alt="profile" className='w-[44px] h-[44px]' />
             <div className='flex flex-col'> 
                 <span>Organized by:</span>
-                <span className='text-[18px]'>Night Club</span>
+                <span className='text-[18px] text-transparent bg-gradient-to-r  from-[#f55e5e]  to-[#7fbde7] bg-clip-text font-bold '>{organizer ? organizer : "Eventora Team"}</span>
             </div>
             <div className='pt-[10px]'> 
                 <button className='bg-black text-white text-[16px] px-[22px] py-1 rounded-[18px]'>
@@ -19,11 +19,11 @@ function EventOrganized() {
             </div>
         </div>
         
-        <div className='pt-4 pl-2'>
+        {/* <div className='pt-4 pl-2'>
             <span  className=''>
             Balloon Nepal Pvt. Ltd. is proud to offer the first-ever commercial hot air balloon flight in Nepal, officially launched in 2018. Since then, our service has been providing unforgettable experiences to tourists in the stunning city of Pokhara, one of Nepal's most popular destinations.
             </span>
-        </div>        
+        </div>         */}
     </div>
 
     
