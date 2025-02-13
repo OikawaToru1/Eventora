a15c7935135e531b6c7e4fb5a77ba52953496c12
Chandan981 <chandantiwari8889@gmail.com>
2025-02-13 14:19:23 +0545

    added children

--- eventora/src/Components/Card/CardSlide.jsx
+++ eventora/src/Components/Card/CardSlide.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from './Card'
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
-function CardSlide() {
+function CardSlide({children}) {
   const slideLeft = () => {
     let slider = document.getElementById('sliderDis');
     slider.scrollLeft = slider.scrollLeft - 250;
@@ -17,13 +17,7 @@ function CardSlide() {
       <div onClick={slideLeft} className="z-10 absolute top-[42%] left-0 w-[32px] h-[54px] rounded-e-[50px] cursor-pointer transition-all"><span className="text-[32px] font-medium absolute top-[13px]"><FaChevronLeft /></span></div>
       <div onClick={slideRight} className="z-10 absolute top-[42%] right-0 w-[32px] h-[54px] rounded-s-[50px] cursor-pointer  transition-all"><span className="absolute top-[13px] text-[32px] font-medium  "><FaChevronRight /></span></div>
       <div className=" w-[100%] h-[100%] overflow-x-scroll scroll-smooth whitespace-nowrap scrollbar-hide flex lg:justify-center mx-auto" id="sliderDis">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        {children}
       </div>
     </div>
   )
