9ae9436916f5848ede1a1112f9be14ca73baafce
Aniket <aniketadhikari2@gmail.com>
2025-02-14 15:50:09 +0545

    added route to icons

--- eventora/src/Components/ExploreOptions.jsx
+++ eventora/src/Components/ExploreOptions.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ExploreIconData from '../assets/DiscovereventData/ExporeData'
+import { Link } from "react-router-dom";
 
 
 function ExploreOptions() {
@@ -9,6 +10,7 @@ function ExploreOptions() {
             <div className=" w-[100%] h-[180px] flex items-center pt-[20px] text-[#2d2d2e] relative ">
                 <div className=" w-[100%] h-[100%] overflow-x-scroll scroll-smooth whitespace-nowrap scrollbar-hide pr-2">
                     {getIcons.map(icon =>(
+                        <Link to={icon.route}>
                         <div key={icon.id} className=" h-[100%] inline-block py-7 px-5 mx-2  text-[16px]">
                             <div className="border-[1px] border-[#2d2d2e] rounded-[50%] w-[68px] h-[68px] flex items-center justify-center hover:drop-shadow-[0_2px_2px_rgba(0,0,0,0.7)] cursor-pointer hover:scale-110 transition-all ">
                                 <img src={icon.IconImage} alt={icon.title} className="w-[24px]" />
@@ -17,6 +19,7 @@ function ExploreOptions() {
                             <span className="w-[50px] text-wrap">{icon.title}</span>
                             </div>
                         </div>
+                        </Link>
                     ))}
                 </div>
             </div>
