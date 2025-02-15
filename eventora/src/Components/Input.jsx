cc39156825c04984ee42b804f5436ab3e3a32f5a
Chandan981 <chandantiwari8889@gmail.com>
2025-02-15 23:58:43 +0545

    removed inline text color for using in other components

--- eventora/src/Components/Input.jsx
+++ eventora/src/Components/Input.jsx
@@ -10,13 +10,13 @@ const Input = React.forwardRef( function Input({
     
 
     return (
-        <div className='w-[80%] flex flex-col mx-auto my-5 lg:pl-5'>
-            {label && <label className='text-xl indent-2  font-medium ' 
+        <div className={`w-[80%] flex flex-col mx-auto my-5 lg:pl-5 ${className}`}>
+            {label && <label className='text-xl indent-2 font-medium' 
             htmlFor={id}>
                 {label}
                 </label>}
                 <input type={type}
-                className= {`px-3 py-2 rounded-lg bg-transparentoutline-none
+                className= {`px-3 py-2 rounded-lg bg-transparent outline-none
                       font-medium border border-gray-200 w-[100%] lg:w-[95%] placeholder:text-[#f0f0f0]
                      ${className}`}
                      ref={ref}
