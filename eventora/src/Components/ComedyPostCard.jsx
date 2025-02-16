6421a4342c322b90aa20458098ef2b639615b652
Aniket <aniketadhikari2@gmail.com>
2025-02-16 21:47:50 +0545

    fixed size for post cards

--- eventora/src/Components/ComedyPostCard.jsx
+++ eventora/src/Components/ComedyPostCard.jsx
@@ -23,7 +23,7 @@ function PostCard({$id, title, featuredImage,location,price,eventType,date}) {
             <div className=" h-[100%] inline-block p-7 lg:p-6"  >
               <div className="hover:drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)] rounded-t-[16px] rounded-b-[16px] transition-all cursor-pointer" >
                 <img src={img}  className="w-[220px] h-[190px] object-cover rounded-t-[16px]" />
-                <div className="bg-[#f3f3f3] w-[220px] rounded-b-[16px] pl-3 py-3">
+                <div className="bg-[#f3f3f3] w-[220px] rounded-b-[16px] pl-3 h-[160px]">
                   <span className="underlineGray">{title}</span>
                   <div className="flex items-center gap-2">
                     <img src={Calender} alt="calender" className="w-5 h-5 "/>
@@ -31,7 +31,7 @@ function PostCard({$id, title, featuredImage,location,price,eventType,date}) {
                   </div>
                   <span className="capitalize underlineGray">{location}</span>
                   {
-                    isExpired(date) && <div><span className='bg-red-700 p-1 rounded-md'>Expired Event</span></div>
+                    isExpired(date) && <div><span className='bg-red-700 p-1 rounded-md'>Event Expired</span></div>
                   }
                  {price ?  <div className="flex justify-end pr-4 text-[22px]">Rs {price}</div> : <div className="flex justify-end pr-4 text-[22px]">Free</div>}
                  <br />
