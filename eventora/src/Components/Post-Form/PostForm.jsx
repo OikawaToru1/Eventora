64de32030cc928b9960d5f6ed9779eaad7863bec
Aniket <aniketadhikari2@gmail.com>
2025-02-16 21:49:26 +0545

    added input feild for ticket url

--- eventora/src/Components/Post-Form/PostForm.jsx
+++ eventora/src/Components/Post-Form/PostForm.jsx
@@ -8,7 +8,7 @@ import { useSelector } from "react-redux";
 import {authService }from '../../appwrite/auth'
 
 export default function PostForm({ post }) {
-    const { register, handleSubmit, watch, setValue, control, getValues } = useForm({
+    const { register, handleSubmit, watch, setValue, control, getValues, formState:{errors} } = useForm({
         defaultValues: {
             title: post?.title || "",
             slug: post?.$id || "",
@@ -18,7 +18,9 @@ export default function PostForm({ post }) {
             eventType : post?.type || "all",
             date: post?.date || "",
             price: post?.price || "",
-            url : post?.url || ""
+            url : post?.url || "",
+            ticketUrl : post?.url || "",
+
         },
     });
 
@@ -50,6 +52,12 @@ export default function PostForm({ post }) {
             if (dbPost) {
                 navigate(`/post/${dbPost.$id}`);
             }
+            // else if (dbPost && eventType=== "concert") {
+            //     navigate(`/concert/${dbPost.$id}`);
+            // }
+            // else if (dbPost && eventType=== "comedy") {
+            //     navigate(`/comedy/${dbPost.$id}`);
+            // }
         } else {
             const file = await appwriteService.uploadFile(data.image[0]);
 
@@ -96,6 +104,7 @@ export default function PostForm({ post }) {
                     className="mb-4"
                     {...register("title", { required: true })}
                 />
+                {errors.title && <span className=' text-red-600 px-20'>Fill the title</span> }
                 <Input
                     label="Slug :"
                     placeholder="Slug"
@@ -111,6 +120,7 @@ export default function PostForm({ post }) {
                     className="mb-4"
                     {...register("location", { required: true })}
                 />
+                {errors.location && <span className=' text-red-600 px-20'>Fill the location</span> }
                 <Input
                     label= "Date :"
                     min = "2024-2-12"
@@ -119,6 +129,7 @@ export default function PostForm({ post }) {
                     className= "mb-4"
                    {...register("date",{required: true})}
                 />
+                {errors.date && <span className=' text-red-600 px-20'>Fill the date</span> }
                 <TextEditor label="Content : " name= "content" control={control} defaultValue={getValues("content")} />
             </div>
             <div className="w-1/3 px-2">
@@ -132,7 +143,7 @@ export default function PostForm({ post }) {
                 {post && (
                     <div className="w-full mb-4">
                         <img
-                            src={appwriteService.getPreview(post.featuredImage)}
+                            src={appwriteService.getFilePreview(post.featuredImage)}
                             alt={post.title}
                             className="rounded-lg"
                         />
@@ -161,6 +172,13 @@ export default function PostForm({ post }) {
                     placeholder = "location"
                     className= "mb-4"
                    {...register("url",{required: true})}
+                />
+                {errors.url && <span className=' text-red-600 px-20'>Enter the location url</span> }
+                <Input
+                    label= "Ticket Url :"
+                    placeholder = "ticket url"
+                    className= "mb-4"
+                   {...register("ticketUrl",)}
                 />
                 <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
                     {post ? "Update" : "Submit"}
