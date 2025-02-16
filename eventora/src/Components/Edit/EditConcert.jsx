7c511b50e2c728555aa409e83c4ca398154ef896
Aniket <aniketadhikari2@gmail.com>
2025-02-16 21:37:50 +0545

    created a edit folder and created files to edit particular category of event

---
+++ eventora/src/Components/Edit/EditConcert.jsx
@@ -0,0 +1,33 @@
+import React,{useEffect,useCallback, useState} from 'react'
+import {Container, PostForm} from '../index'
+import appwriteService from '../../appwrite/config'
+import { useParams, useNavigate } from 'react-router-dom'
+
+function EditPost() {
+    const navigate = useNavigate()
+    const {slug} = useParams();
+    const [post, setPost] = useState(null)
+
+    useEffect(()=>{
+        if(slug)
+        {
+            appwriteService.getConcertPost({slug})
+            .then((post)=>{
+                if(post)
+                {
+                    setPost(post);
+                }
+            })
+        }
+        else{
+            navigate('/home')
+        }
+    },[slug,navigate])
+  return post? (<div>
+    <Container>
+        <PostForm post={post}/>
+    </Container>
+  </div>) : null
+}
+
+export default EditPost
